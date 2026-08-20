a7389a4effd9a4a0966b4d51d89af75b8e47267c
SIr-Isaack <isaackanthony01@gmail.com>
2026-08-20 18:26:51 +0100

    python basics

---
+++ pythonaha/statements.py
@@ -0,0 +1,3 @@
+print("Python is the best in logic hacking")
+print("Hacking is a basic need -skill")
+print("Python and Bash are easy")
---
+++ pythonaha/variables.py
@@ -0,0 +1,4 @@
+# This is a comment
+x = 8
+z = "Hakuna Matata"
+
